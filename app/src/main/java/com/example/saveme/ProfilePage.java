12576181117d1814ad230e2de897de2973664961
erkxw <u2000506@siswa.um.edu.my>
2023-01-02 04:43:54 +0800

    commit

--- app/src/main/java/com/example/saveme/ProfilePage.java
+++ app/src/main/java/com/example/saveme/ProfilePage.java
@@ -2,10 +2,17 @@ package com.example.saveme;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.saveme.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfilePage extends AppCompatActivity {
 
@@ -15,14 +22,21 @@ public class ProfilePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
-        Button yourButton = (Button) findViewById(R.id.change_password_button);
+        Button changePasswordButton = (Button) findViewById(R.id.change_password_button);
 
-        yourButton.setOnClickListener(new View.OnClickListener(){
+        changePasswordButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(ProfilePage.this, ChangePassword.class));
             }
         });
 
+        Button incomeSetupButton = (Button) findViewById(R.id.income_setup_button);
+
+        incomeSetupButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(ProfilePage.this, IncomeSetupPage.class));
+            }
+        });
 
     }
 
