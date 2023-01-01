f7923e3e6dd79f4cd4955e25edf440859e42d57c
acer <harunkamal07@gmail.com>
2023-01-02 04:30:43 +0800

    testing

--- app/src/main/java/com/example/saveme/Test.java
+++ app/src/main/java/com/example/saveme/Test.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class Test extends AppCompatActivity {
 
+    String java = "nick";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
